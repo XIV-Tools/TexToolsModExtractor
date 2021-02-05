3bcaaeca3681fc8c00c5d48cd98b4def2beb8a6c
Yuuki-Walsh <yuuki.walsh@outlook.com>
2021-02-05 20:28:09 +1100

    Initial

---
+++ Extractor/Extractor.cs
@@ -0,0 +1,17 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace TexToolsModExtractor
+{
+	using System;
+	using System.IO;
+
+	public static class Extractor
+	{
+		public static void Extract(string path)
+		{
+			if (!File.Exists(path))
+				throw new FileNotFoundException("Mod pack not found", path);
+		}
+	}
+}
---
+++ ModExtractorCli/Program.cs
@@ -0,0 +1,38 @@
+﻿// © XIV-Tools.
+// Licensed under the MIT license.
+
+namespace TexToolsModExtractorCli
+{
+	using System;
+	using TexToolsModExtractor;
+
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length <= 0)
+			{
+				Console.WriteLine("You must provide the path to a file to extract");
+				return;
+			}
+
+			string file = args[0];
+			Console.WriteLine("Running extractor on file: " + file);
+
+			try
+			{
+				Extractor.Extract(file);
+			}
+			catch (Exception ex)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(ex.Message);
+				Console.ReadKey();
+				return;
+			}
+
+			Console.WriteLine("Extraction complete");
+			Console.ReadKey();
+		}
+	}
+}
