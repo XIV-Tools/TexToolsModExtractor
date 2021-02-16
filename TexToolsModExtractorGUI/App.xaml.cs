44023e7edd79b82fcec75dc27676a862cd9c1c3d
Ani <56663405+Luminiari@users.noreply.github.com>
2021-02-17 03:37:51 +1100

    Some dlls moved to bin folder

--- TexToolsModExtractorGUI/App.xaml.cs
+++ TexToolsModExtractorGUI/App.xaml.cs
@@ -5,7 +5,10 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -16,10 +19,28 @@ namespace TexToolsModExtractorGUI
 	/// </summary>
 	public partial class App : Application
 	{
+		public App()
+		{
+			AssemblyLoadContext.Default.Resolving += this.ResolveAssembly;
+		}
 		protected override void OnStartup(StartupEventArgs e)
 		{
 			XivToolsWpf.Themes.Apply(XivToolsWpf.Brightness.Dark, XivToolsWpf.Colors.DeepOrange);
 			base.OnStartup(e);
 		}
+
+		private Assembly ResolveAssembly(AssemblyLoadContext context, AssemblyName name)
+		{
+			if (name.Name == null)
+				return null;
+
+			string path = AppContext.BaseDirectory + "/bin/" + name.Name + ".dll";
+			if (File.Exists(path))
+				return context.LoadFromAssemblyPath(path);
+
+			return null;
+		}
 	}
+
+
 }
